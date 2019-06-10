6e0107150adb5eea02c963132ea8df2f1c8683f9
tarun dixit <dixittarun81@gmail.com>
2019-06-11 00:52:44 +0530

    first commit

--- app/src/main/java/com/tarun/newsreader/MainActivity.java
+++ app/src/main/java/com/tarun/newsreader/MainActivity.java
@@ -1,9 +1,15 @@
 package com.tarun.newsreader;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
